46c1225d5fdb7e5092517355e4d9d575e6b9b4a1
NicolasDorwald <nicolas.dorwald@gmail.com>
2026-01-13 17:47:58 +0100

    mods css generales, form contact ok

--- back/src/router/router.js
+++ back/src/router/router.js
@@ -4,11 +4,20 @@ import mainController from "../controllers/maincontroller.js";
 export const router = express.Router();
 
 router.get("/", mainController.getRandomCoffees);
-router.get("/catalog", mainController.getCoffeesList);   
-router.get("/entreprise", (req, res) => {res.render("entreprise");});
+
+router.get("/catalog", mainController.getCoffeesList);  
+
+router.get("/company", (req, res) => {
+    res.render("entreprise");});
+
 router.get("/article/:id", mainController.getOneCoffee);
-// router.post('/contact', mainController.postContact);
-// router.get("/contact/confirmation", (req, res) => {res.render("contactConfirmation");});
+
+router.get("/contact", (req, res) => {
+    res.render("contact");});
+
+// router.post('/contact', (req, res) => {
+//     récupérer req.body et envoyer un email en local, Formspree fait le job mantenant
+// });
 
 router.use((req, res) => {
     res.status(404).render('error', { message: "Page non trouvée" });
